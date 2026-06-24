StellaBridge/Bridge-Watch

feat: implement operator notes API, incident heatmap, entity tag sync, and local sandbox harness
Closes #546
Closes #549
Closes #551
Closes #554

- #546: Create local sandbox test harness with mock data, deterministic
  seed script, and reset support for contributor workflows
- #549: Implement operator notes CRUD API with entity linking,
  timestamps, access control, and search support
- #551: Add incident heatmap component with time buckets, severity
  coloring, hover details, date range selection, and accessible labels
- #554: Implement entity tag sync service with cross-entity sync,
  conflict resolution, audit trail, incremental updates, and validation

backend/src/database/models/tag.model.ts
@@ -1,0 +1,157 @@
+import { getDatabase } from "../connection.js";
+
+export interface Tag {
+  id: string;
+  entity_type: string;
+  entity_id: string;
+  tag: string;
+  source: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface TagAuditEntry {
+  id: string;
+  time: Date;
+  entity_type: string;
+  entity_id: string;
+  tag: string;
+  action: string;
+  source: string;
+  metadata: Record<string, unknown> | null;
+}
+
+export class TagModel {
+  private db = getDatabase();
+  private tagsTable = "tags";
+  private auditTable = "tag_audit_log";
+
+  async findByEntity(entityType: string, entityId: string): Promise<Tag[]> {
+    return this.db(this.tagsTable)
+      .where({ entity_type: entityType, entity_id: entityId })
+      .orderBy("created_at", "desc");
+  }
+
+  async findEntitiesByTag(
+    tag: string,
+    entityType?: string
+  ): Promise<Array<{ entity_type: string; entity_id: string }>> {
+    const query = this.db(this.tagsTable)
+      .where({ tag })
+      .select("entity_type", "entity_id")
+      .distinct();
+
+    if (entityType) {
+      query.where("entity_type", entityType);
+    }
+
+    return query;
+  }
+
+  async addTag(
+    entityType: string,
+    entityId: string,
+    tag: string,
+    source = "manual"
+  ): Promise<Tag> {
+    const [existing] = await this.db(this.tagsTable)
+      .where({ entity_type: entityType, entity_id: entityId, tag })
+      .select("id");
+
+    if (existing) {
+      return existing;
+    }
+
+    const [created] = await this.db(this.tagsTable)
+      .insert({ entity_type: entityType, entity_id: entityId, tag, source })
+      .returning("*");
+
+    await this.logAudit(entityType, entityId, tag, "add", source);
+    return created;
+  }
+
+  async removeTag(
+    entityType: string,
+    entityId: string,
+    tag: string,
+    source = "manual"
+  ): Promise<boolean> {
+    const count = await this.db(this.tagsTable)
+      .where({ entity_type: entityType, entity_id: entityId, tag })
+      .del();
+
+    if (count > 0) {
+      await this.logAudit(entityType, entityId, tag, "remove", source);
+    }
+
+    return count > 0;
+  }
+
+  async syncTags(
+    entityType: string,
+    entityId: string,
+    desiredTags: string[],
+    source = "sync"
+  ): Promise<{ added: string[]; removed: string[] }> {
+    const current = await this.findByEntity(entityType, entityId);
+    const currentTags = new Set(current.map((t) => t.tag));
+    const desired = new Set(desiredTags);
+
+    const added: string[] = [];
+    const removed: string[] = [];
+
+    for (const tag of desired) {
+      if (!currentTags.has(tag)) {
+        await this.addTag(entityType, entityId, tag, source);
+        added.push(tag);
+      }
+    }
+
+    for (const tag of currentTags) {
+      if (!desired.has(tag)) {
+        await this.removeTag(entityType, entityId, tag, source);
+        removed.push(tag);
+      }
+    }
+
+    return { added, removed };
+  }
+
+  async getAuditLog(
+    entityType: string,
+    entityId: string,
+    limit = 50
+  ): Promise<TagAuditEntry[]> {
+    return this.db(this.auditTable)
+      .where({ entity_type: entityType, entity_id: entityId })
+      .orderBy("time", "desc")
+      .limit(limit);
+  }
+
+  async getAllTags(): Promise<Array<{ tag: string; count: number }>> {
+    const rows = await this.db(this.tagsTable)
+      .select("tag")
+      .count("id as count")
+      .groupBy("tag")
+      .orderBy("count", "desc");
+    return rows as Array<{ tag: string; count: number }>;
+  }
+
+  private async logAudit(
+    entityType: string,
+    entityId: string,
+    tag: string,
+    action: string,
+    source: string,
+    metadata?: Record<string, unknown>
+  ): Promise<void> {
+    await this.db(this.auditTable).insert({
+      entity_type: entityType,
+      entity_id: entityId,
+      tag,
+      action,
+      source,
+      metadata: metadata ? JSON.stringify(metadata) : null,
+    });
+  }
+}

backend/src/services/incident.service.ts
@@ -1,0 +1,121 @@
+import { getDatabase } from "../database/connection.js";
+import { logger } from "../utils/logger.js";
+
+export interface Incident {
+  id: string;
+  time: Date;
+  entity_type: string;
+  entity_id: string;
+  asset_symbol: string;
+  severity: string;
+  title: string;
+  description: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface HeatmapBucket {
+  date: string;
+  hour: number;
+  count: number;
+  bySeverity: Record<string, number>;
+  incidents: Incident[];
+}
+
+export interface HeatmapData {
+  buckets: HeatmapBucket[];
+  totalIncidents: number;
+  dateRange: { start: string; end: string };
+  assets: string[];
+}
+
+export class IncidentService {
+  private db = getDatabase();
+
+  async getIncidents(params: {
+    startDate?: string;
+    endDate?: string;
+    assetSymbol?: string;
+    severity?: string;
+    limit?: number;
+  }): Promise<Incident[]> {
+    const query = this.db("incidents").orderBy("time", "desc");
+
+    if (params.startDate) {
+      query.where("time", ">=", params.startDate);
+    }
+    if (params.endDate) {
+      query.where("time", "<=", params.endDate);
+    }
+    if (params.assetSymbol) {
+      query.where("asset_symbol", params.assetSymbol);
+    }
+    if (params.severity) {
+      query.where("severity", params.severity);
+    }
+    if (params.limit) {
+      query.limit(params.limit);
+    }
+
+    return query;
+  }
+
+  async getHeatmapData(params: {
+    startDate?: string;
+    endDate?: string;
+    assetSymbol?: string;
+  }): Promise<HeatmapData> {
+    const now = new Date();
+    const defaultStart = new Date(now);
+    defaultStart.setDate(defaultStart.getDate() - 30);
+
+    const startDate = params.startDate ?? defaultStart.toISOString();
+    const endDate = params.endDate ?? now.toISOString();
+
+    const incidents = await this.getIncidents({
+      startDate,
+      endDate,
+      assetSymbol: params.assetSymbol,
+    });
+
+    const bucketMap = new Map<string, HeatmapBucket>();
+    const assets = new Set<string>();
+
+    for (const incident of incidents) {
+      const date = new Date(incident.time);
+      const dateKey = date.toISOString().split("T")[0]!;
+      const hour = date.getHours();
+      const key = `${dateKey}T${String(hour).padStart(2, "0")}`;
+
+      assets.add(incident.asset_symbol);
+
+      if (!bucketMap.has(key)) {
+        bucketMap.set(key, {
+          date: dateKey,
+          hour,
+          count: 0,
+          bySeverity: {},
+          incidents: [],
+        });
+      }
+
+      const bucket = bucketMap.get(key)!;
+      bucket.count++;
+      bucket.bySeverity[incident.severity] =
+        (bucket.bySeverity[incident.severity] ?? 0) + 1;
+      bucket.incidents.push(incident);
+    }
+
+    const buckets = Array.from(bucketMap.values()).sort((a, b) => {
+      if (a.date !== b.date) return a.date.localeCompare(b.date);
+      return a.hour - b.hour;
+    });
+
+    return {
+      buckets,
+      totalIncidents: incidents.length,
+      dateRange: { start: startDate, end: endDate },
+      assets: Array.from(assets).sort(),
+    };
+  }
+}

stellar-kracken/swipely_backend

src/database/models/tag.model.ts
@@ -4,6 +4,10 @@ export interface Tag {
   id: string;
   name: string;
   color: string | null;
+  entity_type?: string;
+  entity_id?: string;
+  tag?: string;
+  source?: string;
   created_at: Date;
   updated_at: Date;
 }
@@ -14,8 +18,21 @@ export interface AssetTag {
   created_at: Date;
 }
 
+export interface TagAuditEntry {
+  id: string;
+  time: Date;
+  entity_type: string;
+  entity_id: string;
+  tag: string;
+  action: string;
+  source: string;
+  metadata: Record<string, unknown> | null;
+}
+
 export class TagModel {
   private db = getDatabase();
+  private tagsTable = "tags";
+  private auditTable = "tag_audit_log";
 
   async findAll(): Promise<Tag[]> {
     return this.db("tags").select("*").orderBy("name", "asc");
@@ -54,8 +71,6 @@ export class TagModel {
   async delete(id: string): Promise<number> {
     return this.db("tags").where("id", id).delete();
   }
-
-  // Association methods
 
   async assign(assetId: string, tagId: string): Promise<void> {
     await this.db("asset_tags")
@@ -120,5 +135,134 @@ export class TagModel {
 
   async clearTagsForAsset(assetId: string): Promise<number> {
     return this.db("asset_tags").where("asset_id", assetId).delete();
+  }
+
+  async findByEntity(entityType: string, entityId: string): Promise<Tag[]> {
+    return this.db(this.tagsTable)
+      .where({ entity_type: entityType, entity_id: entityId })
+      .orderBy("created_at", "desc");
+  }
+
+  async findEntitiesByTag(
+    tag: string,
+    entityType?: string
+  ): Promise<Array<{ entity_type: string; entity_id: string }>> {
+    const query = this.db(this.tagsTable)
+      .where({ tag })
+      .select("entity_type", "entity_id")
+      .distinct();
+
+    if (entityType) {
+      query.where("entity_type", entityType);
+    }
+
+    return query;
+  }
+
+  async addTag(
+    entityType: string,
+    entityId: string,
+    tag: string,
+    source = "manual"
+  ): Promise<Tag> {
+    const [existing] = await this.db(this.tagsTable)
+      .where({ entity_type: entityType, entity_id: entityId, tag })
+      .select("id");
+
+    if (existing) {
+      return existing;
+    }
+
+    const [created] = await this.db(this.tagsTable)
+      .insert({ entity_type: entityType, entity_id: entityId, tag, source })
+      .returning("*");
+
+    await this.logAudit(entityType, entityId, tag, "add", source);
+    return created;
+  }
+
+  async removeEntityTag(
+    entityType: string,
+    entityId: string,
+    tag: string,
+    source = "manual"
+  ): Promise<boolean> {
+    const count = await this.db(this.tagsTable)
+      .where({ entity_type: entityType, entity_id: entityId, tag })
+      .del();
+
+    if (count > 0) {
+      await this.logAudit(entityType, entityId, tag, "remove", source);
+    }
+
+    return count > 0;
+  }
+
+  async syncTags(
+    entityType: string,
+    entityId: string,
+    desiredTags: string[],
+    source = "sync"
+  ): Promise<{ added: string[]; removed: string[] }> {
+    const current = await this.findByEntity(entityType, entityId);
+    const currentTags = new Set(current.map((t) => t.tag));
+    const desired = new Set(desiredTags);
+
+    const added: string[] = [];
+    const removed: string[] = [];
+
+    for (const tag of desired) {
+      if (!currentTags.has(tag)) {
+        await this.addTag(entityType, entityId, tag, source);
+        added.push(tag);
+      }
+    }
+
+    for (const tag of currentTags) {
+      if (!desired.has(tag)) {
+        await this.removeEntityTag(entityType, entityId, tag, source);
+        removed.push(tag);
+      }
+    }
+
+    return { added, removed };
+  }
+
+  async getAuditLog(
+    entityType: string,
+    entityId: string,
+    limit = 50
+  ): Promise<TagAuditEntry[]> {
+    return this.db(this.auditTable)
+      .where({ entity_type: entityType, entity_id: entityId })
+      .orderBy("time", "desc")
+      .limit(limit);
+  }
+
+  async getAllEntityTags(): Promise<Array<{ tag: string; count: number }>> {
+    const rows = await this.db(this.tagsTable)
+      .select("tag")
+      .count("id as count")
+      .groupBy("tag")
+      .orderBy("count", "desc");
+    return rows as Array<{ tag: string; count: number }>;
+  }
+
+  private async logAudit(
+    entityType: string,
+    entityId: string,
+    tag: string,
+    action: string,
+    source: string,
+    metadata?: Record<string, unknown>
+  ): Promise<void> {
+    await this.db(this.auditTable).insert({
+      entity_type: entityType,
+      entity_id: entityId,
+      tag,
+      action,
+      source,
+      metadata: metadata ? JSON.stringify(metadata) : null,
+    });
   }
 }

src/services/incident.service.ts
@@ -65,6 +65,21 @@ export interface CreateIncidentPayload {
   occurredAt?: string;
 }
 
+export interface HeatmapBucket {
+  date: string;
+  hour: number;
+  count: number;
+  bySeverity: Record<string, number>;
+  incidents: BridgeIncident[];
+}
+
+export interface HeatmapData {
+  buckets: HeatmapBucket[];
+  totalIncidents: number;
+  dateRange: { start: string; end: string };
+  assets: string[];
+}
+
 export class IncidentService {
   private db = getDatabase();
 
@@ -186,6 +201,73 @@ export class IncidentService {
       .whereNull("r.id")
       .count<[{ count: string }]>("i.id as count");
     return Number(count);
+  }
+
+  async getHeatmapData(params: {
+    startDate?: string;
+    endDate?: string;
+    assetSymbol?: string;
+  }): Promise<HeatmapData> {
+    const now = new Date();
+    const defaultStart = new Date(now);
+    defaultStart.setDate(defaultStart.getDate() - 30);
+
+    const startDate = params.startDate ?? defaultStart.toISOString();
+    const endDate = params.endDate ?? now.toISOString();
+
+    const filters: IncidentFilters = {
+      assetCode: params.assetSymbol,
+      limit: 10000,
+    };
+
+    const { incidents } = await this.listIncidents(filters);
+
+    const filtered = incidents.filter((inc) => {
+      const occurred = new Date(inc.occurredAt);
+      if (params.startDate && occurred < new Date(params.startDate)) return false;
+      if (params.endDate && occurred > new Date(params.endDate)) return false;
+      return true;
+    });
+
+    const bucketMap = new Map<string, HeatmapBucket>();
+    const assets = new Set<string>();
+
+    for (const incident of filtered) {
+      const date = new Date(incident.occurredAt);
+      const dateKey = date.toISOString().split("T")[0]!;
+      const hour = date.getHours();
+      const key = `${dateKey}T${String(hour).padStart(2, "0")}`;
+
+      if (incident.assetCode) assets.add(incident.assetCode);
+
+      if (!bucketMap.has(key)) {
+        bucketMap.set(key, {
+          date: dateKey,
+          hour,
+          count: 0,
+          bySeverity: {},
+          incidents: [],
+        });
+      }
+
+      const bucket = bucketMap.get(key)!;
+      bucket.count++;
+      bucket.bySeverity[incident.severity] =
+        (bucket.bySeverity[incident.severity] ?? 0) + 1;
+      bucket.incidents.push(incident);
+    }
+
+    const buckets = Array.from(bucketMap.values()).sort((a, b) => {
+      if (a.date !== b.date) return a.date.localeCompare(b.date);
+      return a.hour - b.hour;
+    });
+
+    return {
+      buckets,
+      totalIncidents: filtered.length,
+      dateRange: { start: startDate, end: endDate },
+      assets: Array.from(assets).sort(),
+    };
   }
 
   mapDatabaseRow(row: Record<string, unknown>): BridgeIncident {